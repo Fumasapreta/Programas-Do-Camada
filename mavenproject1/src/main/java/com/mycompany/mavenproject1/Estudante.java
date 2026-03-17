0f6767527fe2048ada83fd82c7aaf881b5c4076b
aluno <aluno@lab01-28>
2026-03-17 11:00:13 -0300

    ?

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudante.java
@@ -0,0 +1,48 @@
+package com.mycompany.mavenproject1;
+import java.util.Date;
+
+public class Estudante {
+        
+        //(ano de ingresso +semetre + 5 digitos)
+        private String codigoMatricula;
+        
+        // Dados Pessoais
+        private String cpf;
+        private String nomeCompleto;
+        private Date dataNascimento;
+        private char genero;
+        private String enderecoEletronico;
+        private String telefoneContato;
+        private static String nome;
+        
+        // Dados Institucionais
+        private int anoIngresso;
+        private int semestreIngresso;
+        private String situacaoAcademica;
+        
+        
+        
+        public Estudante(){
+            this.nome = "Nobody";
+        }
+        
+      public int obterIdade(Date hoje){
+         int idade = 0;
+         
+          //Lógica para calcular idade
+            return idade;   
+           
+  }
+      public static String getNome (){
+     
+          return nome;
+      }     
+      
+      public static void setNome(String _nome){
+          nome = _nome;
+      }
+
+
+
+
+}
